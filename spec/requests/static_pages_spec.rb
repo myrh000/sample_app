c4bbbe15ec12ff5afc6131885135bcfe1dff6631
Ye <dominicyale@126.com>
2014-11-26 14:25:16 +0800

    4.1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,7 +14,7 @@ describe "StaticPages" do
 
 		it "should have the right title" do
 		  visit '/static_pages/home'
-		  expect(page).to have_title("#{base_title} | Home")
+		  expect(page).to have_title("#{base_title}")
 		end
 	end
 
